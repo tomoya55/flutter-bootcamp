2a68b8832aa8b4eb350109f75db62f6bd8192e2e
Tomoya Hirano <hiranotomoya@gmail.com>
2020-05-07 19:20:24 +0900

    Add Slider

--- projects/bmi-calculator-flutter-master/lib/input_page.dart
+++ projects/bmi-calculator-flutter-master/lib/input_page.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'card_icon.dart';
 import 'reusable_card.dart';
+import 'constants.dart';
 
-const activeCardColor = Color(0xFF1D1E33);
-const bottomContainerColor = Color(0xFFEB1555);
-const bottomContainerHeight = 80.0;
+enum Gender {
+  male,
+  female,
+}
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,6 +15,14 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selected;
+  int height = 180;
+
+  Color getColor(Gender g) => (g == Gender.male && selected == Gender.male) ||
+          (g == Gender.female && selected == Gender.female)
+      ? kActiveCardColor
+      : kInactiveCardColor;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,36 +38,93 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReuseableCard(
-                    color: activeCardColor,
-                    cardChild: CardIcon(
-                      iconData: FontAwesomeIcons.mars,
-                      text: 'MALE',
-                    ),
-                  ),
+                      color: getColor(Gender.male),
+                      cardChild: CardIcon(
+                        iconData: FontAwesomeIcons.mars,
+                        text: 'MALE',
+                      ),
+                      onTap: () {
+                        setState(() {
+                          selected = Gender.male;
+                        });
+                      }),
                 ),
                 Expanded(
                   child: ReuseableCard(
-                    color: activeCardColor,
+                    color: getColor(Gender.female),
                     cardChild: CardIcon(
                       iconData: FontAwesomeIcons.venus,
                       text: 'FEMALE',
                     ),
+                    onTap: () {
+                      setState(() {
+                        selected = Gender.female;
+                      });
+                    },
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReuseableCard(color: activeCardColor),
+            child: ReuseableCard(
+              color: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'HEIGHT',
+                    style: kLabelTextStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: kBoldLabelTextStyle,
+                      ),
+                      Text(
+                        'cm',
+                        style: kLabelTextStyle,
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      activeColor: Color(0xFFEB1555),
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double val) {
+                        setState(() {
+                          height = val.toInt();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ReuseableCard(color: activeCardColor),
+                  child: ReuseableCard(color: kActiveCardColor),
                 ),
                 Expanded(
-                  child: ReuseableCard(color: activeCardColor),
+                  child: ReuseableCard(color: kActiveCardColor),
                 ),
               ],
             ),
--- projects/bmi-calculator-flutter-master/lib/reusable_card.dart
+++ projects/bmi-calculator-flutter-master/lib/reusable_card.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
 class ReuseableCard extends StatelessWidget {
-  ReuseableCard({@required this.color, this.cardChild});
+  ReuseableCard({@required this.color, this.cardChild, this.onTap});
 
   final Color color;
   final Widget cardChild;
+  final Function onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    var container = Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: color,
@@ -16,5 +17,10 @@ class ReuseableCard extends StatelessWidget {
       ),
       child: cardChild,
     );
+    if (onTap != null) {
+      return GestureDetector(child: container, onTap: onTap);
+    } else {
+      return container;
+    }
   }
 }
