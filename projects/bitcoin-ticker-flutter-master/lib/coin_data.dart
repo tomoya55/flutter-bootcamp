b320b701af702a177b249db4d89202f91921fd34
Tomoya Hirano <hiranotomoya@gmail.com>
2020-05-25 20:59:42 +0900

    Update multiple rates at one

--- projects/bitcoin-ticker-flutter-master/lib/coin_data.dart
+++ projects/bitcoin-ticker-flutter-master/lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +31,17 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinAPI = 'https://rest.coinapi.io';
+
+class CoinData {
+  Future<dynamic> getCoinData(List<String> bases, String quote) async {
+    var url = '$coinAPI/v1/exchangerate/$quote?invert=true';
+    http.Response resp = await http.get(url,
+        headers: {'X-CoinAPI-Key': '87A8F514-B33F-4EC7-9579-2B157595381F'});
+    if (resp.statusCode == 200) {
+      return jsonDecode(resp.body);
+    } else {
+      print(resp.statusCode);
+    }
+  }
+}
--- projects/bitcoin-ticker-flutter-master/lib/price_screen.dart
+++ projects/bitcoin-ticker-flutter-master/lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +9,64 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  CoinData c = CoinData();
+  List<String> bases = ['BTC', 'ETH', 'LTC'];
+  String selected = currenciesList[0];
+  dynamic data;
+  List<String> rates = ['1', '1', '1'];
+
+  Widget getDropdownButton() {
+    return DropdownButton<String>(
+      items: currenciesList
+          .map(
+            (cur) => DropdownMenuItem(child: Text(cur), value: cur),
+          )
+          .toList(),
+      onChanged: (val) {
+        setState(() {
+          selected = val;
+          loadData();
+        });
+      },
+    );
+  }
+
+  Widget getiOSPicker() {
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (idx) {
+        setState(() {
+          selected = currenciesList[idx];
+          loadData();
+        });
+      },
+      children: currenciesList.map<Text>((cur) => Text(cur)).toList(),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    data = await c.getCoinData(bases, selected);
+    setState(() {
+      data['rates'].forEach((e) {
+        var idx = bases.indexOf(e['asset_id_quote']);
+        if (idx != -1) {
+          if (e['rate'] is int) {
+            rates[idx] = e['rate'].toString();
+          } else {
+            rates[idx] = (e['rate'] as double).toStringAsFixed(2);
+          }
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,34 +76,38 @@ class _PriceScreenState extends State<PriceScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+        children: [
+          ...bases.map((b) {
+            var idx = bases.indexOf(b);
+            return Padding(
+              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+              child: Card(
+                color: Colors.lightBlueAccent,
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+                  child: Text(
+                    '1 ${bases[idx]} = ${rates[idx]} $selected',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
+            );
+          }),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? getiOSPicker() : getDropdownButton(),
           ),
         ],
       ),
